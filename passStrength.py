5bc8b3df6188500593911b3aa571d9d4b29a7530
Josh <joshagrey1@gmail.com>
2025-09-22 19:02:14 -0600

    Basic version of the program completed

--- passStrength.py
+++ passStrength.py
@@ -1,45 +1,67 @@
 import re 
 
-#Password strength checker that checks how strong an inputed password is 
-
-passLength = 0
-passSpecial = False
-passUpper = False
-specialPattern = re.compile(r'[^\w\s]') 
-
-
-userPassword = input("Enter a the password you would like checked: ")
-
 #check diffrent variables of password 
 
 #check if contains uppercase 
-passUpper = any(char.isupper() for char in userPassword)
-print(passUpper)
+def contianUpper(password):
+    return any(char.isupper() for char in password)
 
 
 #check if contains special characters
-if specialPattern.search(userPassword):
-    passSpecial = True
-else:
-    passSpecial = False
 
-print(passSpecial)
+def containSpecial(password):
+
+    specialPattern = re.compile(r'[^\w\s]')  
+
+    if specialPattern.search(password):
+        return True
+    else:
+        return False
+
 
 
 #check length 
-passLength = len(userPassword)
+def checkLength(password):
+    passLength = len(password)
 
-#Amazing 
-if passLength > 13:
-    print("Amazing")
-
-
-#Great 
-if passLength > 8:
-    print("Great")
-
-#Bad
-if passLength < 8:
-    print("bad")
+    #Amazing 
+    if passLength > 13:
+        return 2
 
 
+    #Great 
+    if passLength > 8:
+        return 1
+
+    #Bad
+    if passLength < 8:
+        return 0
+
+
+
+#Give user final review on password 
+def finalPasswordStrengthCheck(password):
+
+    #calculate final strength based on integer score 
+    finalPasswordScore = 0
+    
+    if contianUpper(password):
+        finalPasswordScore = finalPasswordScore + 1
+
+    if containSpecial(password):
+        finalPasswordScore = finalPasswordScore + 1
+
+    finalPasswordScore += checkLength(password)
+
+    
+    if finalPasswordScore >= 4:
+        return "Strong"
+    elif finalPasswordScore == 2:
+        return "Moderate"
+    elif finalPasswordScore <= 1:
+        return "Weak"
+
+#--------------------MAIN------------------------------------------------------------
+
+userPassword = input("Enter a the password you would like checked: ")
+print(finalPasswordStrengthCheck(userPassword))
